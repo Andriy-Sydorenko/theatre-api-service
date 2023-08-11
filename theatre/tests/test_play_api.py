ac838d73b11fda2bc5c7d58b77ad1281e88d8a61
Andriy Sydorenko <sidorenkoandrij217@gmail.com>
2023-08-11 03:49:29 +0300

    added test file for plays

---
+++ theatre/tests/test_play_api.py
@@ -0,0 +1,268 @@
+import os
+import tempfile
+
+from PIL import Image
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APIClient
+
+from theatre.models import Play, Genre, Actor, TheatreHall, Performance
+from theatre.serializers import PlayListSerializer, PlayDetailSerializer
+
+PLAY_URL = reverse("theatre:play-list")
+PERFORMANCE_URL = reverse("theatre:performance-list")
+
+
+def sample_play(**params):
+    defaults = {
+        "title": "Sample play",
+        "description": "Sample description",
+    }
+    defaults.update(params)
+
+    return Play.objects.create(**defaults)
+
+
+def sample_genre(**params):
+    defaults = {
+        "name": "Test genre",
+    }
+    defaults.update(params)
+
+    return Genre.objects.create(**defaults)
+
+
+def sample_actor(**params):
+    defaults = {"first_name": "Test", "last_name": "Actor"}
+    defaults.update(params)
+
+    return Actor.objects.create(**defaults)
+
+
+def sample_performance(**params):
+    theatre_hall = TheatreHall.objects.create(
+        name="Test theatre", rows=20, seats_in_row=20
+    )
+
+    defaults = {
+        "play": None,
+        "theatre_hall": theatre_hall,
+        "show_time": "2022-06-02 14:00:00",
+    }
+    defaults.update(params)
+
+    return Performance.objects.create(**defaults)
+
+
+def image_upload_url(play_id):
+    """Return URL for recipe image upload"""
+    return reverse("theatre:play-upload-image", args=[play_id])
+
+
+def detail_url(play_id):
+    return reverse("theatre:play-detail", args=[play_id])
+
+
+class PlayImageUploadTests(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.user = get_user_model().objects.create_superuser(
+            "admin@myproject.com", "password"
+        )
+        self.client.force_authenticate(self.user)
+        self.play = sample_play()
+        self.genre = sample_genre()
+        self.actor = sample_actor()
+        self.performance = sample_performance(play=self.play)
+
+    def tearDown(self):
+        self.play.image.delete()
+
+    def test_upload_image_to_play(self):
+        """Test uploading an image to play"""
+        url = image_upload_url(self.play.id)
+        with tempfile.NamedTemporaryFile(suffix=".jpg") as ntf:
+            img = Image.new("RGB", (10, 10))
+            img.save(ntf, format="JPEG")
+            ntf.seek(0)
+            res = self.client.post(url, {"image": ntf}, format="multipart")
+        self.play.refresh_from_db()
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertIn("image", res.data)
+        self.assertTrue(os.path.exists(self.play.image.path))
+
+    def test_upload_image_bad_request(self):
+        """Test uploading an invalid image"""
+        url = image_upload_url(self.play.id)
+        res = self.client.post(url, {"image": "not image"}, format="multipart")
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_post_image_to_play_list(self):
+        url = PLAY_URL
+        with tempfile.NamedTemporaryFile(suffix=".jpg") as ntf:
+            img = Image.new("RGB", (10, 10))
+            img.save(ntf, format="JPEG")
+            ntf.seek(0)
+            res = self.client.post(
+                url,
+                {
+                    "title": "Title",
+                    "description": "Description",
+                    "genres": [1],
+                    "actors": [1],
+                    "image": ntf,
+                },
+                format="multipart",
+            )
+
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+        play = Play.objects.get(title="Title")
+        self.assertFalse(play.image)
+
+    def test_image_url_is_shown_on_play_detail(self):
+        url = image_upload_url(self.play.id)
+        with tempfile.NamedTemporaryFile(suffix=".jpg") as ntf:
+            img = Image.new("RGB", (10, 10))
+            img.save(ntf, format="JPEG")
+            ntf.seek(0)
+            self.client.post(url, {"image": ntf}, format="multipart")
+        res = self.client.get(detail_url(self.play.id))
+
+        self.assertIn("image", res.data)
+
+    def test_image_url_is_shown_on_play_list(self):
+        url = image_upload_url(self.play.id)
+        with tempfile.NamedTemporaryFile(suffix=".jpg") as ntf:
+            img = Image.new("RGB", (10, 10))
+            img.save(ntf, format="JPEG")
+            ntf.seek(0)
+            self.client.post(url, {"image": ntf}, format="multipart")
+        res = self.client.get(PLAY_URL)
+
+        self.assertIn("image", res.data[0].keys())
+
+    def test_image_url_is_shown_on_performance_detail(self):
+        url = image_upload_url(self.play.id)
+        with tempfile.NamedTemporaryFile(suffix=".jpg") as ntf:
+            img = Image.new("RGB", (10, 10))
+            img.save(ntf, format="JPEG")
+            ntf.seek(0)
+            self.client.post(url, {"image": ntf}, format="multipart")
+        res = self.client.get(PERFORMANCE_URL)
+
+        self.assertIn("play_image", res.data[0].keys())
+
+
+class UnauthenticatedPlayApiTests(TestCase):
+    def setUp(self) -> None:
+        self.client = APIClient()
+
+    def test_auth_required(self):
+        response = self.client.get(PLAY_URL)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class AuthenticatedPlayApiTests(TestCase):
+    def setUp(self) -> None:
+        self.client = APIClient()
+        self.user = get_user_model().objects.create_user(
+            "test@test.com",
+            "testpassword",
+        )
+        self.client.force_authenticate(self.user)
+
+    def test_plays_list_access(self):
+        sample_play()
+        sample_play()
+
+        response = self.client.get(PLAY_URL)
+
+        plays = Play.objects.all()
+        serializer = PlayListSerializer(plays, many=True)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, serializer.data)
+
+    def test_filter_plays_by_title(self):
+        play1 = sample_play(title="Play1")
+        play2 = sample_play(title="Play2")
+        play3 = sample_play(title="test title")
+
+        response = self.client.get(PLAY_URL, {"title": "pl"})
+
+        serializer1 = PlayListSerializer(play1)
+        serializer2 = PlayListSerializer(play2)
+        serializer3 = PlayListSerializer(play3)
+
+        self.assertIn(serializer1.data, response.data)
+        self.assertIn(serializer2.data, response.data)
+        self.assertNotIn(serializer3.data, response.data)
+
+    def test_filter_plays_by_genres(self):
+        play1 = sample_play(title="Play1")
+        play2 = sample_play(title="Play2")
+        play3 = sample_play(title="play without genre")
+
+        genre1 = sample_genre(name="genre1")
+        genre2 = sample_genre(name="genre2")
+
+        play1.genres.add(genre1)
+        play2.genres.add(genre2)
+
+        response = self.client.get(PLAY_URL, {"genres": f"{genre1.id}, {genre2.id}"})
+
+        serializer1 = PlayListSerializer(play1)
+        serializer2 = PlayListSerializer(play2)
+        serializer3 = PlayListSerializer(play3)
+
+        self.assertIn(serializer1.data, response.data)
+        self.assertIn(serializer2.data, response.data)
+        self.assertNotIn(serializer3.data, response.data)
+
+    def test_filter_plays_by_actors(self):
+        play1 = sample_play(title="Play")
+        play2 = sample_play(title="Play1")
+        play3 = sample_play(title="Play2")
+
+        actor1 = sample_actor()
+        actor2 = sample_actor(first_name="John")
+
+        play1.actors.add(actor1)
+        play2.actors.add(actor2)
+
+        res = self.client.get(PLAY_URL, {"actors": f"{actor1.id},{actor2.id}"})
+
+        serializer1 = PlayListSerializer(play1)
+        serializer2 = PlayListSerializer(play2)
+        serializer3 = PlayListSerializer(play3)
+
+        self.assertIn(serializer1.data, res.data)
+        self.assertIn(serializer2.data, res.data)
+        self.assertNotIn(serializer3.data, res.data)
+
+    def test_retrieve_play_detail(self):
+        play1 = sample_play(title="play11")
+        genre1 = sample_genre(name="genre1")
+
+        play1.genres.add(genre1)
+
+        url = detail_url(play1.id)
+        response = self.client.get(url)
+
+        serializer = PlayDetailSerializer(play1)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, serializer.data)
+
+    def test_create_play_forbidden(self):
+        payload = {
+            "title": "Sample play",
+            "description": "Sample description",
+
+        }
+        response = self.client.post(PLAY_URL, payload)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
