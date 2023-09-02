9d51ef684b6026726174cc3562312100855496bd
Andriy Sydorenko <sidorenkoandrij217@gmail.com>
2023-09-02 14:11:10 +0300

    deleted unnecessary comments from settings.py

--- config/settings.py
+++ config/settings.py
@@ -96,11 +96,6 @@ DATABASES = {
     }
 }
 
-# sqlite3 database
-# "default": {
-#     "ENGINE": "django.db.backends.sqlite3",
-#     "NAME": BASE_DIR / "db.sqlite3",
-# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
